024465493cc61b13298e51524766a9e5fb5653eb
antondemkin <antondemkin@yandex.ru>
2017-06-15 05:52:00 +0300

    added Array Generator for future practice with arrays.

--- arraygen.py
+++ arraygen.py
@@ -1,20 +1,23 @@
 '''
-this script generates am array of given length and range
+this script generates am array of given length, range and it may sort it.
 '''
 
 from random import randint
 
 
-def genArray(length=100, rangeMin=0, rangeMax=255):
+def genArray(length=100, rangeMin=0, rangeMax=255, sorted = False):
     list = []
     for i in range(0, length):
-        list.append(randint(rangeMin, rangeMax))
+        list.append(randint(int(rangeMin), int(rangeMax)))
+    if sorted:
+        list.sort()
     return list
 
 
 def main():
-    for i in range(0, 2):
-        print(genArray(100))
+    print(genArray(100))
+    print(genArray(100,sorted=True))
+
 
 
 if __name__ == "__main__":
