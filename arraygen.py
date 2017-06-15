cbcc8f9b9f23f2b01339ccac8dfd46aa2e02913b
antondemkin <antondemkin@yandex.ru>
2017-06-15 13:54:01 +0300

    removed junk code from array generator

--- arraygen.py
+++ arraygen.py
@@ -5,7 +5,7 @@ this script generates am array of given length, range and it may sort it.
 from random import randint
 
 
-def genArray(length=100, rangeMin=0, rangeMax=255, sorted = False):
+def genArray(length=100, rangeMin=0, rangeMax=255, sorted=False):
     list = []
     for i in range(0, length):
         list.append(randint(rangeMin, rangeMax))
@@ -16,8 +16,7 @@ def genArray(length=100, rangeMin=0, rangeMax=255, sorted = False):
 
 def main():
     print(genArray(100))
-    print(genArray(100,sorted=True))
-
+    print(genArray(100, sorted=True))
 
 
 if __name__ == "__main__":
