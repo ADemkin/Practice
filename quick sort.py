a75442037c33dc47c16fafa9f5f57c0d4e6b053a
antondemkin <antondemkin@yandex.ru>
2017-06-28 18:18:46 +0300

    Quick sort code edited and updated.

--- quick sort.py
+++ quick sort.py
@@ -4,9 +4,6 @@
 
 from arraygen import genArray
 
-unsorted = genArray(50, 0, 100)
-
-
 def quick_sort(list):
     # base case
     if len(list) == 0:
@@ -20,44 +17,40 @@ def quick_sort(list):
     # main logic
     else:
         # переменные
-        lower = []
-        higher = []
         base = list[0]
-        # раскидываем значения по двум спискам
-        for i in list[1:len(list)]:
-            if i < base:
-                lower.append(i)
-                # lower += [i]
-            if i >= base:
-                higher.append(i)
-                # higher += [i]
-                # higher.extend(i)
+ 
+        # новый вариант через list comprehension, спасибо Олегу за подсказку.
+        higher = [ i for i in list[1:len(list)] if i >= base]
+        lower = [i for i in list[1:len(list)] if i < base]
+        
         
         # создаем пустой новый список, к кторому будут присоединены все последующие
         newlist = []
         # костыль, пока не понял как это сделать правильно. TODO: спросить Олега или Игоря как это делать правильно!
         newlower = quick_sort(lower)
         newhigher = quick_sort(higher)
-        
+        # append left part
         if type(newlower) == int:
             newlist.append(newlower)
         else:
             newlist += newlower
-        
+        # append base part
         newlist.append(base)
-        
+        # append higher part
         if type(newhigher) == int:
             newlist.append(newhigher)
         else:
             newlist += newhigher
         # конец костыля
         # ну хоть работает как надо :D TODO: исправить костыль
-        
+
         # возвращаем список
         return newlist
-        
+
         # not working!!!
-        # return quick_sort(lower) + [base] + quick_sort(higher)
+        # update: Не работает даже после изменения кода фильтра. Почему не хочет складывать list и [int]?
+        #return quick_sort(lower) + [base] + quick_sort(higher)
 
 
-print(quick_sort([5, 8, 3, 1, 19, 2, 4, 22, 0, 55]))
+print(quick_sort([5, 8, 3, 1, 19, 2, 4, 22, 0, 55, 34, 6, 42, 12]))
+print(quick_sort(genArray(400,0,1000, False)))
