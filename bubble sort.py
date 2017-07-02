85b06a04ddc32625fd7953f2e450a66dc8c63d9c
antondemkin <antondemkin@yandex.ru>
2017-07-02 21:57:00 +0300

    Added bubble sort algorythm.

---
+++ bubble sort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# Anton Demkin, 2017
+# antondemkin@yandex.ru
+#
+
+from random import randint
+
+unsorted = [randint(0, 100) for i in range(15)]
+
+
+def insertion_sort(arr):
+    for j in range(len(arr) - 1):
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    
+    return arr
+
+
+print(unsorted)
+print(insertion_sort(unsorted))
