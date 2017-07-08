7ba36e12e35535f9993df8226d2ecace00446ddd
antondemkin <antondemkin@yandex.ru>
2017-07-09 02:26:09 +0300

    Внешняя сортировка.

--- File Merging/file merging.py
+++ File Merging/file merging.py
@@ -11,37 +11,70 @@ def create_files():
     Create two files: text1.txt and text2.txt.
     Both files are filled with random numbers separated by new lines.
     '''
+    length = 15
+    gap = 5
+    
+    
     with open('file1.txt', 'w') as f:
         r = 0
-        for i in range(10):
-            r += random.randint(0,5)
+        for i in range(length):
+            r += random.randint(0,gap)
             f.write(str(r) + '\n')
     
     with open('file2.txt', 'w') as f:
         r = 0
-        for i in range(10):
-            r += random.randint(0,5)
+        for i in range(length):
+            r += random.randint(0,gap)
             f.write(str(r) + '\n')
 
 
 create_files()
 
-def merge_files():
+
+def merge_files(file1, file2):
+    '''
+    returns list with file1 and file2 sorted data
+    '''
+    # create new list
+    new_list = []
+    # read first lines
+    next1 = file1.readline()
+    next2 = file2.readline()
+    # check if file end is reached
+    while next1 != '' and next2 != '':
+        if next1 < next2:
+            new_list.append(next1)
+            next1 = file1.readline()
+        else:
+            new_list.append(next2)
+            next2 = file2.readline()
+    return new_list
+
+
+
+
+def open_files():
     '''
     Create file3.txt with numbers from file1.txt and file2.txt
     '''
     with open('file3.txt', 'w') as result:
         with open('file1.txt', 'r') as file1:
             with open('file2.txt', 'r') as file2:
-                for line1, line2 in zip(file1, file2):
-                    # simply merge data, no sorting here
-                    #result.write(str(line1) + str(line2))
-                    # sorting:
-                    if line1 < line2:
-                        result.write(str(line1) + str(line2))
-                    else:
-                        result.write(str(line2) + str(line1))
+                # for line1, line2 in zip(file1, file2):
+                #     # simply merge data, no sorting here
+                #     #result.write(str(line1) + str(line2))
+                #     # sorting:
+                #     if line1 < line2:
+                #         result.write(str(line1) + str(line2))
+                #     else:
+                #         result.write(str(line2) + str(line1))
+                newlist = merge_files(file1, file2)
+                for number in newlist:
+                    result.write(number)
+    #
 
-                    
-merge_files()
+
+
+open_files()
+#merge_files()
 
