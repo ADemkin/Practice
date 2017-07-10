f1001fa1967b899099d352225013b0b100a708de
antondemkin <antondemkin@yandex.ru>
2017-07-11 01:06:57 +0300

    Added two more variations of root calc.

--- get root.py
+++ get root.py
@@ -1,9 +1,12 @@
 # Anton Demkin, 2017
 
 
-def get_root(number):
+def get_root_low_accuracy(number):
+    '''
+    не самая красивая вариация, сравнение чисел выполнено не совсем корректно.
+    '''
     # lower = faster, higher = more accurate
-    accuracy = 5
+    accuracy = 14
     lowest = 0
     highest = number
     middle = (lowest + highest) / 2
@@ -18,8 +21,51 @@ def get_root(number):
     
     
 
-print(get_root(16) == 4)
-print(get_root(64) == 8)
-print(get_root(1024) == 32)
+# print(get_root_low_accuracy(16) == 4)
+# print(get_root_low_accuracy(64) == 8)
+# print(get_root_low_accuracy(1024) == 32)
+#
+# print(get_root_low_accuracy(5645479128))
 
-print(get_root(5645479128))
+def get_root(number):
+    '''
+    Гораздо точнее, используется корректное сравнение float значений
+    '''
+    # high accuracy
+    eps = 0.000000000000001
+    # high speed
+    #eps = 0.000000001
+    low = 0
+    high = number
+    root = (low + high) / 2
+    while abs(root ** 2 - number) > eps:
+        # binary search
+        if root ** 2 > number:
+            high = root
+        if root ** 2 < number:
+            low = root
+        root = (low + high) / 2
+        #print(root)
+    return root
+
+
+#print(get_root(35) ** 2)
+#print(get_root(74) ** 2)
+#print(get_root(123) ** 2)
+
+def get_root_faster(number):
+    eps = 0.00000000000001
+    low = 0
+    high = number
+    root = ( low + high ) / 2
+    while abs(low - high) > eps:
+        if root ** 2 > number:
+            high = root
+        if root ** 2 < number:
+            low = root
+        root = ( low + high ) / 2
+    return root
+
+
+print(get_root_faster(35), get_root(35))
+print(get_root_faster(36), get_root(36))
